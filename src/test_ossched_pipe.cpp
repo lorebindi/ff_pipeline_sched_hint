269ad38e9f329d7247f329377380bd3b25d3cd45
bindi <l.bindi4@studenti.unipi.it>
2024-09-19 08:06:56 +0000

    Added update_thread_priorities(pthread, int) using sched_priority

--- src/test_ossched_pipe.cpp
+++ src/test_ossched_pipe.cpp
@@ -136,7 +136,7 @@ private:
      * @param queue_lengths pipeline operators' output queue length.
      * @return the logical priority for each operator.
      */
-    vector<double>* get_logical_priority(vector<unsigned long>* queue_lengths) {
+    const vector<double>* get_logical_priority(vector<unsigned long>* queue_lengths) {
         vector<double>* logical_priorities = new vector<double>(queue_lengths->size());
 
         /*
@@ -144,10 +144,11 @@ private:
          *
          */
 
+    	// delete queue_lengths;
         return logical_priorities;
     }
 
-	vector<int>* translation (vector<double>* logical_priority) {
+	const vector<int>* translation (vector<double>* logical_priority) {
     	vector<int>* concrete_priorities = new vector<int>(logical_priority->size());
 
     	/*
@@ -155,7 +156,46 @@ private:
 		 *
 		 */
 
+    	//delete logical_priority;
     	return concrete_priorities;
+    }
+
+    /**
+	* Note from Linux Man: "for threads scheduled under one of the normal scheduling policies
+	* (SCHED_OTHER, SCHED_IDLE, SCHED_BATCH), sched_priority is not used in scheduling
+	* decisions (it must be specified as 0). Processes scheduled under one of the real-time
+	* policies (SCHED_FIFO, SCHED_RR) have a sched_priority value in the range 1 (low) to 99 (high)."
+    *
+    * @param thread thread whose priority we want to change.
+    * @param new_priority the new value of priority
+    * @return 0 on success, -1 on failure
+    */
+    int update_thread_priorities(pthread_t thread, int new_priority) {
+    	int policy;
+    	struct sched_param param;
+
+    	// get the scheduling policy and parameters for the specified thread
+    	if (pthread_getschedparam(thread, &policy, &param) != 0) {
+    		std::cerr << "Error getting scheduling parameters: " << std::strerror(errno) << std::endl;
+    		return -1;
+    	}
+
+    	// Checking the value of new_priority
+    	int min_priority = sched_get_priority_min(policy);
+    	int max_priority = sched_get_priority_max(policy);
+    	if (new_priority > max_priority || new_priority < min_priority) {
+    		std::cerr << "New priority is out of range (" << min_priority << " to " << max_priority << ")." << std::endl;
+    		return -1;
+    	}
+
+    	// set the new priority
+    	param.sched_priority = new_priority;
+    	if (pthread_setschedparam(thread, policy, &param) != 0) {
+    		std::cerr << "Error setting scheduling parameters: " << std::strerror(errno) << std::endl;
+    		return -1;
+    	}
+
+    	return 0;
     }
 
 	void apply_priorities(vector<int>* concrete_priorities) {
@@ -165,13 +205,15 @@ private:
     	assert (pipe_nodes.size() == concrete_priorities->size());
 
 	    for(size_t i = 0; i < concrete_priorities->size()-1; i++) {
-		    pthread_t t = pipe_nodes[i]->get_handle();
+		    this->update_thread_priorities(pipe_nodes[i]->get_handle(), (*concrete_priorities)[i]);
 	    }
+
+    	//delete concrete_priorities;
     }
 
 public:
     Manager(ff_pipeline& p, std::chrono::milliseconds i)
-        : pipe(p), interval(i), outFile("logs/out.log") {
+        : pipe(p), interval(i), outFile("../logs/out.log") {
         if (!outFile.is_open()) {
             cerr << "Error opening output file" << endl;
             exit(EXIT_FAILURE);
@@ -192,10 +234,18 @@ public:
                 return;
             }
 
+        	/*
+        	 * Call to:
+        	 *
+        	 * get_logical_priority
+        	 *
+        	 * translation
+        	 *
+        	 * apply priorities
+        	 *
+        	 */
 
         }
-
-
 
         std::printf("manager completed\n");
     }
@@ -239,7 +289,7 @@ int main(int argc, char* argv[]) {
 		pipe.add_stage(new Stage(2000*i, cpuset), true);
 	pipe.add_stage(&last);
 
-	// setta tutte le code a bounded di capacità 10
+	// set all queues to bounded of capacity 10
 	// pipe.setXNodeInputQueueLength(10, true);
 
 	// lancio il thread manager
